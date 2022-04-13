ac98607bf64b2286ef630136f5e1e509e9c708a3
jinhengxi <96713050+jinhengxi@users.noreply.github.com>
2022-04-13 11:49:24 +0900

    tristich
    comemate quiz

---
+++ main.js
@@ -0,0 +1,12 @@
+function 위(){
+    var p = document.getElementsByTagName('p')
+    p[0].innerHTML = "위코드"
+}
+function 코(){
+    var p = document.getElementsByTagName('p')
+    p[1].innerHTML = "코드"
+}
+function 드(){
+    var p = document.getElementsByTagName('p')
+    p[2].innerHTML = "드드"
+}
